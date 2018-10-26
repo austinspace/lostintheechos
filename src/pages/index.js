ef24692f431903a1bc863edd1fedceaffc1f78ed
austinspace <austin.watling@gmail.com>
2018-10-26 12:43:10 -0600

    rev 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
 
-export default () => <div>Hello Gatsby!</div>
+export default () => ( 
+<div style={{ color: `purple` }}>
+<h1>Hello Gatsby!</h1>
+<p>What a world.</p>
+<p>Hi Gabi!</p>
+<p>This is what is called hot reloading</p>
+<img src="https://source.unsplash.com/random/400x200" alt="" />
+</div>
+)
